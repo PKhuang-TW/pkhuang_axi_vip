ab68eb739485a6a2586f44e8c89867d60e19afe9
PKhuang-TW <pokaihuang.tw@gmail.com>
2026-05-03 23:39:48 +0800

    Fix: Add missing seqr creation

--- src/axi_virtual_sequencer.sv
+++ src/axi_virtual_sequencer.sv
@@ -10,6 +10,12 @@ class axi_virtual_sequencer extends uvm_sequencer;
     function new ( string name = "axi_virtual_sequencer", uvm_component parent );
         super.new(name, parent);
     endfunction
+
+    function build_phase ( uvm_phase phase );
+        super.build_phase(phase);
+        seqr_mst = axi_master_sequencer :: type_id :: create ("seqr_mst", this);
+        seqr_slv = axi_slave_sequencer :: type_id :: create ("seqr_slv", this);
+    endfunction
     
 endclass
 
